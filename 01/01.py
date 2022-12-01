f2129f456acde44eb5f18394269cf4c7b5bae0c2
Jolly Hare <91928992+jolly-hare@users.noreply.github.com>
2022-12-01 21:52:08 +0000

    Update 01.py
    cleaned up

--- 01/01.py
+++ 01/01.py
@@ -1,39 +1,19 @@
-
 def parse(file_input):
-    input_file = [i.strip() for i in file_input.readlines()]
-    return [line for line in input_file]
-
-
-def part1(data):
-    elves = list()
-    cumulativesum = 0
-    for i in range(0, len(data)):
-        if data[i] == '':
-            elves.append(cumulativesum)
-            cumulativesum = 0
-        else:
-            cumulativesum += int(data[i])
-    return max(elves)
-
-
-def part2(data):
-    elves = list()
-    cumulativesum = 0
-    for i in range(0, len(data)):
-        if data[i] == '':
-            elves.append(cumulativesum)
-            cumulativesum = 0
-        else:
-            cumulativesum += int(data[i])
-    elves_sorted = sorted(elves, reverse=True)
-    return sum(elves_sorted[:3])
+    input_lines = [i.strip() for i in file_input.readlines()]
+    return input_lines
 
 
 def solve(puzzle_input):
-    # data = parse(puzzle_input)
-    solution1 = part1(puzzle_input)
-    solution2 = part2(puzzle_input)
-    return solution1, solution2
+    elves = list()
+    cumulativesum = 0
+    for i in range(0, len(puzzle_input)):
+        if puzzle_input[i] == '':
+            elves.append(cumulativesum)
+            cumulativesum = 0
+        else:
+            cumulativesum += int(puzzle_input[i])
+    elves_sorted = sorted(elves, reverse=True)
+    return max(elves), sum(elves_sorted[:3])
 
 
 if __name__ == "__main__":
@@ -41,4 +21,4 @@ if __name__ == "__main__":
         puzzle_parsed = parse(f)
     print(f'parse check: {puzzle_parsed[:10]}')
     solutions = solve(puzzle_parsed)
-    print(f'\npart 1: {solutions[0]}\npart 2: {solutions[1]}')
+    print(f'\n{"*"*20}\n  part 1: {solutions[0]}\n  part 2: {solutions[1]}\n{"*"*20}')
