1723ae1533a04d762ff488bd800e80ab58c48445
Jolly Hare <91928992+jolly-hare@users.noreply.github.com>
2022-12-02 13:42:01 +0000

    Update newday.py

--- newday.py
+++ newday.py
@@ -12,8 +12,18 @@ else:
     day_zeroed = day_num
 if not os.path.exists(day_zeroed):
     os.mkdir(day_zeroed)
-    shutil.copy('0X.example', os.path.join(day_zeroed, day_zeroed + '.example'))
-    shutil.copy('empty.py', os.path.join(day_zeroed, day_zeroed + '.py'))
+    inputfile = os.path.join(day_zeroed, day_zeroed + '.input')
+    examplefile = os.path.join(day_zeroed, day_zeroed + '.example')
+    solutionfile = os.path.join(day_zeroed, day_zeroed + '.py')
+    if not os.path.exists(examplefile):
+        shutil.copy('0X.example', examplefile)
+    if not os.path.exists(solutionfile):
+        shutil.copy('empty.py', solutionfile)
     resp = requests.get(f'https://adventofcode.com/2022/day/{str(day_num)}/input', cookies=cookie, headers=header)
-    with open(f"{os.path.join(day_zeroed, day_zeroed + '.input')}", 'w') as f:
-        f.write(resp.text.strip("\n"))
+    if not os.path.exists(inputfile):
+        with open(f"{inputfile}", 'w') as f:
+            f.write(resp.text.strip("\n"))
+    else:
+        print('input file exists, not requesting again')
+else:
+    print("that day's directory exists, skipping")
