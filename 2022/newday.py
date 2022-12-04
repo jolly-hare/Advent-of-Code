e44bed9a749af5ca72f3b6b21892a474188691e9
Jolly Hare <91928992+jolly-hare@users.noreply.github.com>
2022-12-04 03:52:57 +0000

    Update newday.py
    add dynamic dates

--- 2022/newday.py
+++ 2022/newday.py
@@ -3,8 +3,9 @@ import shutil
 import requests
 
 day_num = "3"
-cookie = {'session': 'SESSION'}
-header = {"User-Agent": "USER AGENT INFO - REPO VERSION EMAIL USERNAME"}
+
+cookie = {'session': '53616c7465645f5f5a1042cc185996f38cbee45ad106b68477257c0431043ab0f717899549dab741f633a39c654f9bdc03c75364140b53ca0a861dd99f44d972'}
+header = {"User-Agent": "github.com/jolly-hare/Advent-of-Code-2022; version 1.0; contact twitter @ts_hare"}
 if len(day_num) == 1:
     day_zeroed = "0" + day_num
 else:
@@ -17,7 +18,11 @@ if not os.path.exists(day_zeroed):
     if not os.path.exists(examplefile):
         shutil.copy('0X.example', examplefile)
     if not os.path.exists(solutionfile):
-        shutil.copy('aoc_template.py', solutionfile)
+        with open('aoc_template.py') as f:
+            tmp = f.read()
+            tmp2 = tmp.replace("$0X", day_zeroed)
+        with open(solutionfile, "w") as f2:
+            f2.write(tmp2)
     if not os.path.exists(inputfile):
         resp = requests.get(f'https://adventofcode.com/2022/day/{str(day_num)}/input', cookies=cookie, headers=header)
         with open(f"{inputfile}", 'w') as f:
