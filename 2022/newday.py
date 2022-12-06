ad498502c5eea4599ec468885ca3b457b8467848
Jolly Hare <91928992+jolly-hare@users.noreply.github.com>
2022-12-06 16:05:23 +0000

    Update newday.py

--- 2022/newday.py
+++ 2022/newday.py
@@ -2,8 +2,10 @@ import os
 import shutil
 import requests
 
-day_num = "3"
+year_num = "2022"
+day_num = "6"
 
+url = f'https://adventofcode.com/{year_num}/day/{day_num}/input'
 cookie = {'session': 'SESSION cookie'}
 header = {'User-Agent': 'repo, version, your contact details'}
 if len(day_num) == 1:
@@ -12,22 +14,24 @@ else:
     day_zeroed = day_num
 if not os.path.exists(day_zeroed):
     os.mkdir(day_zeroed)
+    print(f'new directory created for day {day_zeroed}')
     inputfile = os.path.join(day_zeroed, day_zeroed + '.input')
     examplefile = os.path.join(day_zeroed, day_zeroed + '.example')
     solutionfile = os.path.join(day_zeroed, day_zeroed + '.py')
     if not os.path.exists(examplefile):
         shutil.copy('0X.example', examplefile)
+        print(f'new empty example file created as: {examplefile}')
     if not os.path.exists(solutionfile):
         with open('aoc_template.py') as f:
             tmp = f.read()
             tmp2 = tmp.replace("$0X", day_zeroed)
         with open(solutionfile, "w") as f2:
             f2.write(tmp2)
+            print(f'new solution template file created as: {solutionfile}')
     if not os.path.exists(inputfile):
-        resp = requests.get(f'https://adventofcode.com/2022/day/{str(day_num)}/input', cookies=cookie, headers=header)
+        resp = requests.get(url=url, cookies=cookie, headers=header)
         with open(f"{inputfile}", 'w') as f:
             f.write(resp.text.strip("\n"))
-    else:
-        print('input file exists, not requesting again')
+            print(f'downloaded input data for day {day_zeroed}')
 else:
     print("that day's directory exists, skipping")
